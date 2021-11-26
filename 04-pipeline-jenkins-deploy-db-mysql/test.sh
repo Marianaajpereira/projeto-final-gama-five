2854a682e021dda7c1f5ffb5c443da37ef0b45b5
Marianaajpereira <mariana.ajpereira@hotmail.com>
2021-11-26 04:24:12 +0000

    teste criação infra instancias sql 15

--- 04-pipeline-jenkins-deploy-db-mysql/test.sh
+++ 04-pipeline-jenkins-deploy-db-mysql/test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-cd 01-pipeline-jenkins-deploy-ami-k8s/00-terraform
+cd  04-pipeline-jenkins-deploy-db-mysql/00-terraform
 uri=$(terraform output | grep private_ip | awk '{print $2;exit}' | sed -e "s/\",//g")
 
 echo $uri
 
-ssh -i /var/lib/jenkins/.ssh/kp-gamafive.pem ubuntu@$uri -oStrictHostKeyChecking=no << EOF
-echo \$(pwd)
-version=\$(kubectl version)
-echo "\$version"
-if [[ ! -z "\$version" ]];
-then 
-    echo "::::: Kubernets instalado :::::"
-    exit 0
-else
-    echo "::::: Kubernets não instalado :::::"
-    exit 1
-fi
-EOF
+# ssh -i /var/lib/jenkins/.ssh/kp-gamafive.pem ubuntu@$uri -oStrictHostKeyChecking=no << EOF
+# echo \$(pwd)
+# version=\$(kubectl version)
+# echo "\$version"
+# if [[ ! -z "\$version" ]];
+# then 
+#     echo "::::: Kubernets instalado :::::"
+#     exit 0
+# else
+#     echo "::::: Kubernets não instalado :::::"
+#     exit 1
+# fi
+# EOF
