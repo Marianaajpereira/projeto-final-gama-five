b840cee8c95a66ad3c2585036827963fdc89c5ed
Marianaajpereira <mariana.ajpereira@hotmail.com>
2021-11-26 02:21:25 +0000

    teste criação infra instancias sql 3

--- 04-pipeline-jenkins-deploy-db-mysql/00-terraform/main.tf
+++ 04-pipeline-jenkins-deploy-db-mysql/00-terraform/main.tf
@@ -20,7 +20,7 @@ resource "aws_route_table" "route_table_privada"{
   }
 }
 
-resource "aws_route_table_association" "associação subnets à route table privada" {
+resource "aws_route_table_association" "associação subnets a route table privada" {
   for_each = aws_subnet.subnets_privadas_db
   subnet_id      = each.subnet_id
   route_table_id = aws_route_table.route_table_privada.id
