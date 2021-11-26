0d01d6cfcb71496b4d6ac0279b27cdd8adaae95b
Marianaajpereira <mariana.ajpereira@hotmail.com>
2021-11-26 05:10:35 +0000

    Adicionando pipeline de deploys do cluster k8s

--- 03-pipeline-jenkins-deploy-cluster-k8s/00-terraform/locals.tf
+++ 03-pipeline-jenkins-deploy-cluster-k8s/00-terraform/locals.tf
@@ -3,19 +3,8 @@ locals {
 
   number_of_private_subnets = length(aws_subnet.k8s_subnets_private)
 
-  number_of_public_subnets = length(aws_subnet.subnets_public)
-
-  public_subnet_ids_list = flatten([
-    for subnet in aws_subnet.subnets_public : subnet.id
-  ])
-
   private_subnet_ids_list = flatten([
     for subnet in aws_subnet.k8s_subnets_private : subnet.id
   ])
 
-  nat_eip_ids_list = flatten([
-    for eip in aws_eip.nat_gtw_public_ips : eip.id
-  ])
-
-  association_eip_nat_gtw = zipmap(local.public_subnet_ids_list, local.nat_eip_ids_list)
 }
