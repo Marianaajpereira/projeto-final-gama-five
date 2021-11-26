4ebcc61505c2668b09ac19dc6c2df85edbd682de
Marianaajpereira <mariana.ajpereira@hotmail.com>
2021-11-26 02:15:31 +0000

    teste criação infra instancias sql 2

--- 04-pipeline-jenkins-deploy-db-mysql/deploy_terraform.sh
+++ 04-pipeline-jenkins-deploy-db-mysql/deploy_terraform.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cd 01-pipeline-jenkins-deploy-ami-k8s/00-terraform
+# cd 02-pipeline-jenkins-deploy-ami-db-mysql/02-terraform-ami
+# ami=$(terraform output | grep AMI | awk '{print $2;exit}' | sed -e "s/\",//g")
+
+
+cd 04-pipeline-jenkins-deploy-db-mysql/00-terraform
 terraform init
 terraform apply -auto-approve
 
 echo "Aguardando criação de maquinas ..."
-sleep 10 # 10 segundos
+sleep 20 # 10 segundos
